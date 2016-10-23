a91a93034c91b1ed8e5a53da242836d05bbf4ec9
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-23 11:37:36 +0100

    Arreglando errores

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -2,8 +2,7 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var shell = require('gulp-shell');
-var iaas = require('./lib/lib.js');
-var json = ('./package.json');
+var json = require('./package.json');
 
 gulp.task('deploy', function() {
   return gulp.src(['_book/*/*/*','_book/*/*','_book/*'])
